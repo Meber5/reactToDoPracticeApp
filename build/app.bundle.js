2c203447187a05fb31e22ac937db2446038df342
Meber5 <meberle@exeter.edu>
2017-06-30 14:36:37 -0700

    prints list

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -9772,6 +9772,8 @@ module.exports = __webpack_require__(18);
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(82);
 
 var _react2 = _interopRequireDefault(_react);
@@ -9782,11 +9784,58 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_reactDom2.default.render(_react2.default.createElement(
-   'p',
-   null,
-   'React lives!'
-), document.getElementById('root'));
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var dummyData = ["Walk the dogs", "Go surfing", "send it"];
+
+var ToDo = function ToDo(props) {
+  return _react2.default.createElement(
+    'li',
+    { key: props.task },
+    _react2.default.createElement(
+      'button',
+      null,
+      'X'
+    ),
+    props.task
+  );
+};
+
+var ToDoList = function (_React$Component) {
+  _inherits(ToDoList, _React$Component);
+
+  function ToDoList(props) {
+    _classCallCheck(this, ToDoList);
+
+    return _possibleConstructorReturn(this, (ToDoList.__proto__ || Object.getPrototypeOf(ToDoList)).call(this, props));
+  }
+
+  _createClass(ToDoList, [{
+    key: 'render',
+    value: function render() {
+      console.log('test');
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+          'ul',
+          null,
+          this.props.data.map(function (item) {
+            return _react2.default.createElement(ToDo, { task: item });
+          })
+        )
+      );
+    }
+  }]);
+
+  return ToDoList;
+}(_react2.default.Component);
+
+_reactDom2.default.render(_react2.default.createElement(ToDoList, { data: dummyData }), document.getElementById('root'));
 
 /***/ }),
 /* 84 */
--- reactApp/app.js
+++ reactApp/app.js
@@ -3,5 +3,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+var dummyData = ["Walk the dogs", "Go surfing", "send it"];
+
+const ToDo = (props) => <li key={props.task}><button>{'X'}</button>{props.task}</li>;
+
+class ToDoList extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    console.log('test');
+    return(
+      <div>
+        <ul>
+          {this.props.data.map((item)=> <ToDo task={item} />)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<ToDoList data={dummyData} />,
+    document.getElementById('root'));
